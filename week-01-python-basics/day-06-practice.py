906fa9ddd5df216cb51a7aee0d844e3891003cd7
Qunoot Ahmed <qunootahmed4@gmail.com>
2026-06-12 01:02:17 +0500

    day 6, added functions and exercises

--- week-01-python-basics/day-06-practice.py
+++ week-01-python-basics/day-06-practice.py
@@ -1,5 +1,9 @@
 # range object
 # you can iterate over range
+from enum import unique
+from idlelib.debugobj import myrepr
+from multiprocessing.reduction import duplicate
+
 print(range(100))
 
 for number in range(0,100):
@@ -36,3 +40,50 @@ for pic in picture:
         elif item == 1:
             print('*', end=' ')
     print()
+
+# Exercise: Check for duplicates in list:
+some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+
+duplicates = []
+for value in some_list:
+    if some_list.count(value) > 1:
+        if value not in duplicates:
+            duplicates.append(value)
+print(duplicates)
+
+# function
+
+def say_hello():
+    print('hello')
+say_hello()
+
+def isOddOrEven(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+print(isOddOrEven(88))
+
+# *args **kwargs
+def super_func(*args, **kwargs):
+    total = 0
+    for items in kwargs.values():
+        total += items
+    return sum(args) + total
+print(super_func(1, 2, 3, 4, 5, num1=5, num2=10))
+
+# Exercise: Highest even number
+def highest_even(li):
+    even = []
+    for item in li:
+        if item % 2 == 0:
+            even.append(item)
+        else:
+            pass
+    even.sort()
+    return even[-1]
+
+print(highest_even([10, 2, 3, 4, 8, 11]))
+# we can also use max function
+
+# walrus operator --> :=
